5c65432b68bc07818bf074159f1a72b2110ccf56
Mary <maryandreeva100@gmail.com>
2023-08-02 01:29:07 +0300

    Complete project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
   return (
     <nav className="blue darken-2">
       <div className="nav-wrapper">
-        <a href="#" className="brand-logo">
+        <a href="#!" className="brand-logo">
           React Shop
         </a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
